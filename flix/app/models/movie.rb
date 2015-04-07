f4b4602500062153a2e9bd48192ad87e090e4f6f
nasnick <nas.nick@gmail.com>
2015-04-07 20:56:03 +1200

    remove old files

--- flix/app/models/movie.rb
+++
@@ -1,5 +0,0 @@
-class Movie < ActiveRecord::Base
-  def flop?
-    total_gross == blank? || total_gross < 400000000
-  end
-end
--- flix/db/migrate/20150323080341_add_fields_to_movie.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToMovie < ActiveRecord::Migration
-  def change
-    add_column :movies, :description, :text
-    add_column :movies, :released_on, :date
-  end
-end
