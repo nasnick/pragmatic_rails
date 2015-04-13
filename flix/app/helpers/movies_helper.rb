1da84462cdeee18693f1b5a029a3d6f47020d934
nasnick <nas.nick@gmail.com>
2015-04-13 20:04:16 +1200

    Mon 13 Apr 2015 20:04:16 NZST

--- flix/app/helpers/movies_helper.rb
+++ flix/app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+  def check_movie_success(movie)
+    if movie.flop?
+      content_tag(:strong, "FLOP!")
+    else
+      number_to_currency(movie.total_gross) 
+    end
+  end
 end
--- flix/spec/features/show_movie_spec.rb
+++ flix/spec/features/show_movie_spec.rb
@@ -12,4 +12,27 @@ describe "Shows the contents of the show page" do
    expect(page).to have_text(movie.description)
    expect(page).to have_text(movie.released_on)
   end
+  
+  describe "displays total gross" do
+    it "shows the total gross if the movie exceeds 50M" do
+    
+      movie = Movie.create(movie_attributes(total_gross: 60000000.00))
+
+      visit movie_url(movie)
+
+      expect(page).to have_text("$60,000,000.00")
+  end
+
+
+  describe "displays total gross" do
+    it "shows 'flop!' if the total gross is less than 50M" do
+    
+      movie = Movie.create(movie_attributes(total_gross: 49000000.00))
+
+      visit movie_url(movie)
+
+      expect(page).to have_text("FLOP!")
+    end
+   end
+  end
 end
