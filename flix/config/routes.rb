7d674c1dd09c65e4fce8ea0476518c6d4ad7ecdb
System Administrator <root@nickschofield.local>
2015-04-14 20:48:27 +1200

    Tue Apr 14 20:48:27 NZST 2015

--- flix/config/routes.rb
+++ flix/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "movies#index"
   get 'movies' => 'movies#index' 
   get "movies/:id" => "movies#show", as: "movie"
+  get "movies/battleship" => "movies#battleship", as: "battleship"
 end
