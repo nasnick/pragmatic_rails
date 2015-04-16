ab87f7695db14e19ded93783d481b967bc208afb
nasnick <nas.nick@gmail.com>
2015-04-16 19:21:55 +1200

    Thu 16 Apr 2015 19:21:55 NZST

--- flix/config/routes.rb
+++ flix/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "movies#index"
   get 'movies' => 'movies#index' 
   get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" =>  "movies#edit", as: "movie_edit"
+  get "movies/:id/edit" =>  "movies#edit", as: "edit_movie"
 end
+
+#'app.movies_url' => "http://www.example.com/movies"', 'app.movies_path' => '/movies' app.movie_edit_path(3) => '/movies/3/edit'
