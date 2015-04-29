82e0c89cad657674253d28c70e1e174e025fb88c
nasnick <nas.nick@gmail.com>
2015-04-29 21:35:07 +1200

    Wed 29 Apr 2015 21:35:07 NZST

--- flix/spec/support/attributes.rb
+++ flix/spec/support/attributes.rb
@@ -4,6 +4,10 @@ def movie_attributes(overrides = {})
     rating: "PG-13",
     total_gross: 318412101.00,
     description: "Tony Stark builds an armored suit to fight the throes of evil",
-    released_on: "2008-05-02"
+    released_on: "2008-05-02",
+    cast: "The award-winning cast",
+    director: "The ever-creative director",
+    duration: "123 mins",
+    image_file_name: "movie.png"
   }.merge(overrides)
 end
