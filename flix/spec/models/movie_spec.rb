631b95b6dcb09a9413689979c473cfeb1ec0dcf0
nasnick <nas.nick@gmail.com>
2015-05-05 21:44:42 +1200

    Tue  5 May 2015 21:44:42 NZST

--- flix/spec/models/movie_spec.rb
+++ flix/spec/models/movie_spec.rb
@@ -33,4 +33,23 @@ describe "A movie" do
    
     expect(Movie.released).to eq([movie3, movie2, movie1])
   end
+  it "has many reviews" do
+    movie = Movie.new(movie_attributes)
+
+    review1 = movie.reviews.new(review_attributes)
+    review2 = movie.reviews.new(review_attributes)
+
+    expect(movie.reviews).to include(review1)
+    expect(movie.reviews).to include(review2)
+  end
+
+  it "deletes associated reviews" do
+    movie = Movie.create(movie_attributes)
+
+    movie.reviews.create(review_attributes)
+
+    expect {
+      movie.destroy
+    }.to change(Review, :count).by(-1)
+  end
 end
